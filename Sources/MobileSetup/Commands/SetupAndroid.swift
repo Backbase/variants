bb373b55fc14ccb15046c41b38887a9447f5391d
Arthur Alves <arthura@backbase.com>
2020-04-14 13:38:43 +0200

    chore: add Android command

---
+++ Sources/MobileSetup/Commands/SetupAndroid.swift
@@ -0,0 +1,40 @@
+//
+//  File.swift
+//  
+//
+//  Created by Arthur Alves on 14/04/2020.
+//
+
+import Foundation
+import PathKit
+import SwiftCLI
+
+final class SetupAndroid: Command, VerboseLogger {
+    let name = "android"
+    let shortDescription = "Setup multiple build flavours for Android project, alongside fastlane"
+    
+    @Param var configuration: String
+    
+    @Flag("-c", "--config", description: "Use a yaml configuration file")
+    var isValidConfigurationFile: Bool
+    
+    @Flag("-f", "--include-fastlane", description: "Should setup fastlane")
+    var includeFastlane: Bool
+    
+    func execute() throws {
+        guard isValidConfigurationFile else {
+            throw CLI.Error(message: "Error: Use '-c' to specify the configuration file")
+        }
+        
+        let configurationPath = Path(configuration)
+        guard !configurationPath.isDirectory else {
+            throw CLI.Error(message: "Error: \(configurationPath) is a directory path")
+        }
+        
+        if includeFastlane {
+            log("Including Fastlane", indentationLevel: 2)
+        }
+        
+        log("Done project setup!", indentationLevel: 2)
+    }
+}
