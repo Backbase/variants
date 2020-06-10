d7c9158237b5ad3959867086838b2953d914c61b
Arthur Alves <arthuralves.p@gmail.com>
2020-06-10 10:25:00 +0200

    xcconfig values from 'Variant' corrected based on variant type (default or custom)

--- Sources/Variants/Schemas/Environment.swift
+++ Sources/Variants/Schemas/Environment.swift
@@ -9,24 +9,42 @@ import Foundation
 
 public struct Variant: Codable {
     let name: String
-    let id_suffix: String
+    let id_suffix: String?
     let version_name: String
     let version_number: Int
     let custom: [String: String]?
     
     func getDefaultValues(for target: Target) -> [String: String] {
         var customDictionary: [String: String] = [
-            "MV_APP_NAME": target.name+" "+name,
-            "MV_BUNDLE_ID": target.bundleId+"."+id_suffix,
-            "MV_VERSION_NAME": version_name,
-            "MV_VERSION_NUMBER": String(version_number)
+            "V_APP_NAME": target.name+configName,
+            "V_BUNDLE_ID": target.bundleId+configIdSuffix,
+            "V_VERSION_NAME": version_name,
+            "V_VERSION_NUMBER": String(version_number)
         ]
        
         custom?.forEach({ (key, value) in
-            customDictionary["MV_\(key.uppercased())"] = value
+            customDictionary["\(key.uppercased())"] = value
         })
         
         return customDictionary
+    }
+    
+    var configName: String {
+        switch name {
+        case "default":
+            return ""
+        default:
+            return " "+name
+        }
+    }
+    
+    var configIdSuffix: String {
+       switch name {
+        case "default":
+            return ""
+        default:
+            return id_suffix != nil ? "."+id_suffix! : ""
+        }
     }
 }
 
