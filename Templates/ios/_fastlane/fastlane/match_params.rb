87e70fdd84ef6e9ebde21f7ab7fc1136432243b9
Arthur Alves <arthuralves.p@gmail.com>
2020-06-12 15:33:59 +0200

    move most logic to their own file

--- Templates/ios/_fastlane/fastlane/match_params.rb
+++
@@ -1,8 +0,0 @@
-
-# REPLACE WITH ENVIRONMENT VARIABLES
-
-# Name and password for Match temporary keychain
-MATCH_PARAMS = {
-  MATCH_KEYCHAIN_NAME: 'match-keychain',
-  MATCH_KEYCHAIN_PASSWORD: '77hamcj819oak',
-}.freeze
