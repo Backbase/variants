8adf59499ffc37ab732eb284900af0ff624a80a4
Arthur Alves <arthuralves.p@gmail.com>
2020-06-12 15:33:59 +0200

    move most logic to their own file

---
+++ Templates/ios/_fastlane/fastlane/parameters/match_params.rb
@@ -0,0 +1,14 @@
+
+# REPLACE WITH ENVIRONMENT VARIABLES
+
+# Name and password for Match temporary keychain
+MATCH_PARAMS = {
+  MATCH_KEYCHAIN_NAME: 'sample-match-keychain', # ENV['MATCH_KEYCHAIN_NAME']
+  MATCH_KEYCHAIN_PASSWORD: 'sample-77hamcj819oak', # ENV['MATCH_KEYCHAIN_PASSWORD']
+  
+  # This is needed if your Match repository is private
+  MATCH_GIT_BASIC_AUTHORIZATION: ENV['MATCH_GIT_BASIC_AUTHORIZATION'],
+  
+  # Match repository password, used to decrypt files
+  MATCH_PASSWORD: ENV['MATCH_PASSWORD']
+}.freeze
--- Templates/ios/_fastlane/fastlane/parameters/project_params.rb
+++ Templates/ios/_fastlane/fastlane/parameters/project_params.rb
@@ -5,12 +5,13 @@ PROJECT_PARAMS = {
   test_scheme: 'ragnarok-us-app',
   uitest_scheme: 'ragnarok-us-app-ui-tests',
   derived_data_path: '.derivedData',
-  reports_folder: './reports',
   ui_test_reports_folder: './ui-test-results',
   swiftlint_path: './.swiftlint.yml',
   test_devices: ['iPhone 11'],
   ui_test_destination: "platform=iOS Simulator,name=#{ENV["DEVICE"]},OS=#{ENV["OS"]}",
   ui_test_plan: 'Regression',
   workspace: 'ragnarok-app.xcworkspace',
-  coherent_config: '../coherent-swift.yml'
+  
+  REPORTS_FOLDER: './reports',
+  COHERENT_SPEC: '../coherent-swift.yml'
 }.freeze
