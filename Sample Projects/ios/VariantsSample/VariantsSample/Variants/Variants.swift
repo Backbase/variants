7f7a873aedb1460ffbb1b0ff7c49ad23fce5cad3
Paolo Di Lorenzo <paolo@backbase.com>
2020-10-28 16:06:19 -0400

    Add variants to sample project

---
+++ Sample Projects/ios/VariantsSample/VariantsSample/Variants/Variants.swift
@@ -0,0 +1,17 @@
+//
+//  Variants
+//
+//  Copyright (c) Backbase B.V. - https://www.backbase.com
+//  Created by Arthur Alves
+//
+
+import Foundation
+
+public struct Variants {
+    static let configuration: [String: Any] = {
+        guard let infoDictionary = Bundle.main.infoDictionary else {
+            fatalError("Info.plist file not found")
+        }
+        return infoDictionary
+    }()
+}
---
+++ Sample Projects/ios/VariantsSample/fastlane/parameters/project_params.rb
@@ -0,0 +1,13 @@
+
+PROJECT_PARAMS = {
+    # MUST CHANGE
+    APP_ICON_SET_PATH: "../SamplePath/Assets.xcassets/AppIcon.appiconset",
+    XCCONFIG_PATH: "../SamplePath/Variants/variants.xcconfig",
+    
+    COHERENT_SPEC: '../coherent-swift.yml',
+    VARIANTS_SPEC: '../variants.yml',
+    SWIFTLINT_PATH: 'swiftlint.yml',
+    DERIVED_DATA_PATH: '.derivedData',
+    REPORTS_FOLDER: './reports',
+    TEST_DEVICES: ['iPhone 11']
+}.freeze
