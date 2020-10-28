ba6f7d5f017fdcff0b65a6feb571d1f26bf4f4de
Paolo Di Lorenzo <paolo@backbase.com>
2020-10-28 14:12:31 -0400

    Add Cocoapods

---
+++ Sample Projects/ios/VariantsSample/Pods/Target Support Files/Pods-VariantsSample-VariantsSampleUITests/Pods-VariantsSample-VariantsSampleUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_VariantsSample_VariantsSampleUITests : NSObject
+@end
+@implementation PodsDummy_Pods_VariantsSample_VariantsSampleUITests
+@end
---
+++ Sample Projects/ios/VariantsSample/Pods/Target Support Files/Pods-VariantsSampleTests/Pods-VariantsSampleTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_VariantsSampleTests : NSObject
+@end
+@implementation PodsDummy_Pods_VariantsSampleTests
+@end
