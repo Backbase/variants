1c2e30a6a25f95247b35259b63678ed986483052
Arthur Alves <arthuralves.p@gmail.com>
2020-10-29 11:23:21 +0100

    tests: add executable test for usage

--- Tests/VariantsTests/VariantsTests.swift
+++ Tests/VariantsTests/VariantsTests.swift
@@ -2,31 +2,50 @@ import XCTest
 import class Foundation.Bundle
 
 final class VariantsTests: XCTestCase {
-    func testExample() throws {
-//        // This is an example of a functional test case.
-//        // Use XCTAssert and related functions to verify your tests produce the correct
-//        // results.
-//
-//        // Some of the APIs that we use below are available in macOS 10.13 and above.
-//        guard #available(macOS 10.13, *) else {
-//            return
-//        }
-//
-//        let fooBinary = productsDirectory.appendingPathComponent("Variants")
-//
-//        let process = Process()
-//        process.executableURL = fooBinary
-//
-//        let pipe = Pipe()
-//        process.standardOutput = pipe
-//
-//        try process.run()
-//        process.waitUntilExit()
-//
-//        let data = pipe.fileHandleForReading.readDataToEndOfFile()
-//        let output = String(data: data, encoding: .utf8)
-//
-//        XCTAssertEqual(output, "Hello, world!\n")
+    func testUsage() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+
+        // Some of the APIs that we use below are available in macOS 10.13 and above.
+        guard #available(macOS 10.13, *) else {
+            return
+        }
+
+        let fooBinary = productsDirectory.appendingPathComponent("Variants")
+
+        let process = Process()
+        process.executableURL = fooBinary
+
+        let pipe = Pipe()
+        process.standardOutput = pipe
+
+        try process.run()
+        process.waitUntilExit()
+
+        let data = pipe.fileHandleForReading.readDataToEndOfFile()
+        let output = String(data: data, encoding: .utf8)
+
+        let expectedOutput =
+            """
+            OVERVIEW: A command-line tool to setup deployment variants and working CI/CD
+            setup
+
+            USAGE: variants <subcommand>
+
+            OPTIONS:
+              --version               Show the version.
+              -h, --help              Show help information.
+
+            SUBCOMMANDS:
+              init                    Generate spec file - variants.yml
+              setup                   Setup deployment variants (alongside Fastlane)
+              switch                  Switch variants
+
+              See \'variants help <subcommand>\' for detailed help.
+
+            """
+        XCTAssertEqual(output, expectedOutput)
     }
 
     /// Returns path to the built products directory.
@@ -42,6 +61,6 @@ final class VariantsTests: XCTestCase {
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testUsage", testUsage),
     ]
 }
