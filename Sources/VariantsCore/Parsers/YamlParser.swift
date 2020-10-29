93db1beb1e04592554a8e73a2c6581eda9842cca
Arthur Alves <arthuralves.p@gmail.com>
2020-10-29 11:22:58 +0100

    chore: clean up

--- Sources/VariantsCore/Parsers/YamlParser.swift
+++ Sources/VariantsCore/Parsers/YamlParser.swift
@@ -13,32 +13,25 @@ class YamlParser {
         let decoder = YAMLDecoder()
         let encoder = YAMLEncoder()
         
-//        do {
-            let encodedYAML = try String(contentsOfFile: configurationPath, encoding: .utf8)
-            let decoded: Configuration = try decoder.decode(Configuration.self, from: encodedYAML)
-            
-            Logger.shared.logInfo("Loading configuration", item: "")
-            var encoded = try encoder.encode(decoded)
-            
-            switch platform {
-            case .ios:
-                encoded = try encoder.encode(decoded.ios)
-            case .android:
-                encoded = try encoder.encode(decoded.android)
-            default: break
-            }
-            
-            let nsString = encoded as NSString
-            nsString.enumerateLines { (stringLine, _) in
-                Logger.shared.log(item: stringLine, indentationLevel: 1, color: .purple, logLevel: .verbose)
-            }
-            
-            return decoded
-            
-//        } catch {
-//            Logger.shared.logError(item: (error as NSError).debugDescription)
-//            throw RuntimeError(error.localizedDescription)
-//
-//        }
+        let encodedYAML = try String(contentsOfFile: configurationPath, encoding: .utf8)
+        let decoded: Configuration = try decoder.decode(Configuration.self, from: encodedYAML)
+        
+        Logger.shared.logInfo("Loading configuration", item: "")
+        var encoded = try encoder.encode(decoded)
+        
+        switch platform {
+        case .ios:
+            encoded = try encoder.encode(decoded.ios)
+        case .android:
+            encoded = try encoder.encode(decoded.android)
+        default: break
+        }
+        
+        let nsString = encoded as NSString
+        nsString.enumerateLines { (stringLine, _) in
+            Logger.shared.log(item: stringLine, indentationLevel: 1, color: .purple, logLevel: .verbose)
+        }
+        
+        return decoded
     }
 }
