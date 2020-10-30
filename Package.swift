e4be7b702862a73bcd0977caa8a57428a469cd9f
Arthur Alves <arthuralves.p@gmail.com>
2020-10-30 14:22:23 +0100

    ref: attempt to resolve dependency issues

--- Package.swift
+++ Package.swift
@@ -7,8 +7,35 @@ let package = Package(
     products: [
         .executable(name: "variants", targets: ["Variants"])
     ],
-    dependencies: [],
+    dependencies: [
+        .package(
+            url: "https://github.com/kylef/PathKit",
+            from: "1.0.0"
+        ),
+        .package(
+            url: "https://github.com/jpsim/Yams.git",
+            from: "2.0.0"
+        ),
+        .package(
+            name: "XcodeProj",
+            url: "https://github.com/tuist/xcodeproj.git",
+            from: "7.11.1"
+        ),
+        .package(
+            url: "https://github.com/apple/swift-argument-parser.git",
+            from: "0.1.0"
+        ),
+    ],
     targets: [
+        .target(
+            name: "VariantsCore",
+            dependencies: [
+                "PathKit",
+                "Yams",
+                "XcodeProj",
+                .product(name: "ArgumentParser", package: "swift-argument-parser")
+            ]
+        ),
         .target(
             name: "Variants",
             dependencies: [
