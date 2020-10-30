7adac9c369847ad8419f8f7c33f4e9e70447b00f
Arthur Alves <arthuralves.p@gmail.com>
2020-10-30 13:53:35 +0100

    ref: adjust dependencies between CLI and Core targets

--- Package.swift
+++ Package.swift
@@ -7,43 +7,12 @@ let package = Package(
     products: [
         .executable(name: "variants", targets: ["Variants"])
     ],
-    dependencies: [
-        .package(
-            url: "https://github.com/kylef/PathKit",
-            from: "1.0.0"
-        ),
-        .package(
-            url: "https://github.com/jpsim/Yams.git",
-            from: "2.0.0"
-        ),
-        .package(
-            name: "XcodeProj",
-            url: "https://github.com/tuist/xcodeproj.git",
-            from: "7.11.1"
-        ),
-        .package(
-            url: "https://github.com/apple/swift-argument-parser.git",
-            from: "0.1.0"
-        ),
-    ],
+    dependencies: [],
     targets: [
-        .target(
-            name: "VariantsCore",
-            dependencies: [
-                "PathKit",
-                "Yams",
-                "XcodeProj",
-                .product(name: "ArgumentParser", package: "swift-argument-parser")
-            ]
-        ),
         .target(
             name: "Variants",
             dependencies: [
-                "VariantsCore",
-                "PathKit",
-                "Yams",
-                "XcodeProj",
-                .product(name: "ArgumentParser", package: "swift-argument-parser")
+                "VariantsCore"
             ]
         ),
         .testTarget(
