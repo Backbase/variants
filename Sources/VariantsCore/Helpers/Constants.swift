503797c22ab60fb3577cfe2ca2b2aa9d8b6013fd
Arthur Alves <arthuralves.p@gmail.com>
2020-11-04 16:44:05 +0100

    chore: enrich constant files with template file names

--- Sources/VariantsCore/Helpers/Constants.swift
+++ Sources/VariantsCore/Helpers/Constants.swift
@@ -16,4 +16,13 @@ struct StaticPath {
         static let parametersFolder = Path("fastlane/parameters/")
         static let variantsParametersFileName = "variants_params.rb"
     }
+    
+    struct Gradle {
+        static let variantsScriptFileName = "variants.gradle"
+    }
+    
+    struct Template {
+        static let variantsScriptFileName = "variants-template.gradle"
+        static let fastlaneParametersFileName = "variants_params_template.rb"
+    }
 }
