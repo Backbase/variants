95d6d6807b4dc9373f575cfa3822285f45e37eeb
Arthur Alves <arthuralves.p@gmail.com>
2020-11-04 16:45:01 +0100

    feat: 'variants.gradle' script is now generated using a Stencil template

--- Sources/VariantsCore/Factory/Android/GradleScriptFactory.swift
+++ Sources/VariantsCore/Factory/Android/GradleScriptFactory.swift
@@ -7,144 +7,74 @@
 
 import Foundation
 import PathKit
+import Stencil
 
 class GradleScriptFactory {
+    init(templatePath: Path? = try? TemplateDirectory().path) {
+        self.templatePath = templatePath
+    }
     
     /// Create `gradleScripts/variants.gradle` file inside project's path
     /// - Parameters:
     ///   - configuration: Android configuration from `variants.yml`
     ///   - variant: Desired project variant.
-    func createScript(with configuration: AndroidConfiguration, variant: AndroidVariant) {
-        var gradleFileContent = ""
-        
-        //Write the variant data
-        gradleFileContent.appendLine("// ==== Variant values ==== ")
-        gradleFileContent.addGradleDefinition("versionName", value: variant.versionName)
-        gradleFileContent.addGradleValueDefinition("versionCode", value: variant.versionCode)
-        gradleFileContent.addGradleDefinition("appIdentifier",
-                                              value: configuration.appIdentifier+variant.configIdSuffix)
-        gradleFileContent.addGradleDefinition("appName", value: configuration.appName+variant.configName)
-        
-        if let signing = configuration.signing {
-            //Write the signing data
-            gradleFileContent.appendLine("\n// ==== Signing values ==== ")
-            gradleFileContent.addGradleDefinition("signingKeyAlias", value: signing.keyAlias)
-            gradleFileContent.addGradleDefinition("signingKeyPassword", value: signing.keyPassword)
-            gradleFileContent.addGradleDefinition("signingStoreFile", value: signing.storeFile)
-            gradleFileContent.addGradleDefinition("signingStorePassword", value: signing.storePassword)
+    func createScript(with configuration: AndroidConfiguration, variant: AndroidVariant) throws {
+        let variantsScriptPath = try Path(configuration.path).safeJoin(path: Path("gradleScripts/"))
+        if !variantsScriptPath.exists {
+            try variantsScriptPath.mkpath()
         }
-        
-        // Append custom gradle properties
-        gradleFileContent = appendGradleProperties(variant.custom,
-                                                   using: "\n// ==== Variant custom values ==== ",
-                                                   to: gradleFileContent)
-        
-        gradleFileContent = appendGradleProperties(configuration.custom,
-                                                   using: "\n// ==== Custom values ==== ",
-                                                   to: gradleFileContent)
-        
-        // Write wrapper gradle tasks
-        gradleFileContent.appendLine("\n// ==== Wrapper gradle tasks ==== ")
-        
-        gradleFileContent.addWrapperGradleTasks([
-            WrapperGradleTask(name: "vBuild", dependsOnTaskWithName: variant.taskBuild,
-                              description: "Wrapper Gradle task used for building the application"),
-            WrapperGradleTask(name: "vUnitTests", dependsOnTaskWithName: variant.taskUnitTest,
-                              description: "Wrapper Gradle task used for executing the Unit Tests"),
-            WrapperGradleTask(name: "vUITests", dependsOnTaskWithName: variant.taskUitest,
-                              description: "Wrapper Gradle task used for executing the UI Tests")
-        ])
-        
-        //Write the actual files
-        gradleFileContent.writeGradleScript(with: configuration)
+        guard let data = try render(with: configuration, variant: variant) else { return }
+        try write(data, using: variantsScriptPath)
     }
     
-    /// Append properties whose destination is '.project' as gradle properties
-    /// to a string content
-    /// - Parameters:
-    ///   - properties: Optional array of CustomProperty
-    ///   - header: String header preceding the properties
-    ///   - content: String where these properties will be appended to
-    /// - Returns: New content containing the gradle properties added
-    func appendGradleProperties(_ properties: [CustomProperty]?,
-                                using header: String,
-                                to content: String) -> String {
-        
-        // Add Properties to Gradle file
-        let gradleProperties = properties?.filter { $0.destination == .project } ?? []
-        guard !gradleProperties.isEmpty else { return content }
-        var mutableContent = content
-        mutableContent.appendLine(header)
-        gradleProperties.forEach { property in
-            mutableContent.addGradleDefinition(property.name, value: property.value)
-        }
-        return mutableContent
-    }
-}
+    func render(with configuration: AndroidConfiguration, variant: AndroidVariant) throws -> Data? {
+        var context = [
+            "variant": variant,
+            "configuration": configuration
+        ] as [String : Any]
 
-private struct WrapperGradleTask{
-    let name: String
-    let dependsOnTaskWithName: String
-    let description: String
-}
-
-fileprivate extension String {
-    func writeGradleScript(with configuration: AndroidConfiguration) {
-        let fm = FileManager.default
-        let destinationFolderPath = configuration.path + "/gradleScripts"
-        let destionationFilePath = destinationFolderPath + "/variants.gradle"
-        
-        do {
-            try fm.createDirectory(atPath: destinationFolderPath,
-                                   withIntermediateDirectories: true, attributes: nil)
-            fm.createFile(atPath: destionationFilePath,
-                          contents: self.data(using: .utf8), attributes: nil)
-        } catch {
-            Logger.shared.logError(item: """
-                Could not generate gradle script:\n\(error.localizedDescription)
-            """)
+        if let variantProperties = variant.custom?.filter({ $0.destination == .project }) {
+            context["variant_properties"] = variantProperties
         }
+        
+        if let globalProperties = configuration.custom?.filter({ $0.destination == .project }) {
+            context["global_properties"] = globalProperties
+        }
+        
+        guard
+            let path = templatePath,
+            let androidTemplatePath = try? path.safeJoin(path: Path("android/"))
+        else { return nil }
+        let environment = Environment(loader: FileSystemLoader(paths: [androidTemplatePath.absolute()]))
+        let rendered = try environment.renderTemplate(name: StaticPath.Template.variantsScriptFileName,
+                                                      context: context)
+        
+        // Replace multiple empty lines by one only
+        let lines = rendered.split(whereSeparator: \.isNewline)
+        let content = lines.joined(separator: "\n")
+        
+        print(content)
+        return Data(content.utf8)
     }
     
-    mutating func addWrapperGradleTasks(_ tasks: [WrapperGradleTask]) {
-        let dependsOnScript = #"""
-        %@if (task.name == "%@") {
-                %@.dependsOn(task)
-            }
-        """#
-        
-        var dependsOnScriptList = ""
-        
-        for (index, element) in tasks.enumerated() {
-            self.appendLine(String(format: "def %@ = task %@", element.name, element.name))
-            let isFirst = index == 0
-            if isFirst {
-                dependsOnScriptList.append(String(format: dependsOnScript, "",
-                                                  element.dependsOnTaskWithName, element.name))
+    func write(_ data: Data, using gradleScriptFolder: Path) throws {
+            if gradleScriptFolder.isDirectory, gradleScriptFolder.exists {
+                let fastlaneParametersFile = Path(gradleScriptFolder.string+StaticPath
+                                                    .Gradle.variantsScriptFileName)
                 
+                // Only proceed to write to file if such doesn't yet exist
+                // Or does exist and 'isWritable'
+                guard !fastlaneParametersFile.exists
+                        || fastlaneParametersFile.isWritable else {
+                    throw TemplateDoesNotExist(templateNames: [gradleScriptFolder.string])
+                }
+                
+                // Write to file
+                try fastlaneParametersFile.write(data)
             } else {
-                dependsOnScriptList.append(String(format: dependsOnScript, " else ",
-                                                  element.dependsOnTaskWithName, element.name))
+                throw TemplateDoesNotExist(templateNames: [gradleScriptFolder.string])
             }
-        }
-        
-        let whenTaskAddedScript = """
-        tasks.whenTaskAdded { task ->
-            %@
-        }
-        """
-        
-        self.appendLine(String(format: whenTaskAddedScript, dependsOnScriptList))
     }
     
-    mutating func addGradleDefinition(_ name: String, value: String) {
-        self.appendLine("rootProject.ext.\(name) = \"\(value.envVarValue() ?? value)\"")
-    }
-    
-    // This will add the value without quotes
-    // This is useful in cases of versionCode which should be
-    // represented as an int and not a string.
-    mutating func addGradleValueDefinition(_ name: String, value: String) {
-        self.appendLine("rootProject.ext.\(name) = \(value.envVarValue() ?? value)")
-    }
+    private let templatePath: Path?
 }
