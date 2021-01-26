c358bef02b52f3bb6c551c805689c2236984c027
Arthur Alves <arthuralves.p@gmail.com>
2021-01-26 12:54:02 +0100

    refactor: when returning custom properties that read from environment variables, add it's specific project syntax on their respective factories

--- Sources/VariantsCore/Extensions/CustomProperty+EnvironmentVar.swift
+++ Sources/VariantsCore/Extensions/CustomProperty+EnvironmentVar.swift
@@ -23,7 +23,7 @@ extension CustomProperty {
                     let envVarName = String(value[envVarNameIndex])
                     switch destination {
                     case .project:
-                        return (isEnvVar: true, string: "System.getenv('"+envVarName+"')")
+                        return (isEnvVar: true, string: envVarName)
                     case .fastlane:
                         return (isEnvVar: true, string: "ENV[\""+envVarName+"\"]")
                     }
