481d0f65aaf7d22c339e7506c8ae7bcedd5ba460
oleg <oleg@backbase.com>
2022-03-18 18:41:51 +0200

    fix: add missing copyright

--- Sources/VariantsCore/Loggers/StandardOutputStream.swift
+++ Sources/VariantsCore/Loggers/StandardOutputStream.swift
@@ -1,7 +1,7 @@
 //
-//  StandardOutputStream.swift
-//  VariantsCoreTests
+//  Variants
 //
+//  Copyright (c) Backbase B.V. - https://www.backbase.com
 //  Created by Abdoelrhman Eaita on 17/09/2021.
 //
 
