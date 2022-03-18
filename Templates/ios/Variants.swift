1d2f51133578b1867b2fa4b5d3c18d47a4c34a1b
oleg <oleg@backbase.com>
2022-03-18 19:06:39 +0200

    Update Variants.swift

--- Templates/ios/Variants.swift
+++ Templates/ios/Variants.swift
@@ -4,9 +4,7 @@
 //  Copyright (c) Backbase B.V. - https://www.backbase.com
 //  Created by Arthur Alves
 //
-
 import Foundation
-
 public struct Variants {
     static let configuration: [String: Any] = {
         guard let infoDictionary = Bundle.main.infoDictionary else {
