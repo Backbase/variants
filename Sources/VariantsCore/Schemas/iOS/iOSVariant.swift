e3f3f08120c7f5c89d4e37c5c8bba4ce58812c9b
Gabriel Minucci <gabriel@minucci.com.br>
2022-05-02 22:46:06 +0200

    feat: add option to define bundle_id in variants config

--- Sources/VariantsCore/Schemas/iOS/iOSVariant.swift
+++ Sources/VariantsCore/Schemas/iOS/iOSVariant.swift
@@ -11,77 +11,111 @@ import Foundation
 
 public struct iOSVariant: Variant {
     let name: String
-    let app_icon: String?
-    let id_suffix: String?
-    let version_name: String
-    let version_number: Int
+    let versionName: String
+    let versionNumber: Int
+    let appIcon: String?
+    let storeDestination: Destination
     let signing: iOSSigning?
     let custom: [CustomProperty]?
-    internal let store_destination: String?
+    private let idSuffix: String?
+    private let bundleID: String?
+     
+    public var title: String { name }
     
-    public var title: String { return name }
+    var configName: String {
+        guard name != "default" else { return "" }
+        return " " + name
+    }
+    
+    var destinationProperty: CustomProperty {
+        CustomProperty(
+            name: "STORE_DESTINATION",
+            value: storeDestination.rawValue,
+            destination: .fastlane
+        )
+    }
+    
+    init(from unnamediOSVariant: UnnamediOSVariant, name: String, globalSigning: iOSSigning?) throws {
+        guard globalSigning != nil || unnamediOSVariant.signing != nil else {
+            throw RuntimeError(
+                """
+                Variant "\(name)" doesn't contain a 'signing' configuration. \
+                Create a global 'signing' configuration or make sure all variants have this property.
+                """)
+        }
+        
+        if let localSigning = unnamediOSVariant.signing, let globalSigning = globalSigning {
+            self.signing = try localSigning ~ globalSigning
+        } else if let localSigning = unnamediOSVariant.signing {
+            self.signing = localSigning
+        } else {
+            self.signing = globalSigning
+        }
+        
+        let hasIDSuffixAndBundleID = unnamediOSVariant.idSuffix != nil && unnamediOSVariant.bundleId != nil
+        let hasNoIDSuffixOrBundleID = unnamediOSVariant.idSuffix == nil && unnamediOSVariant.bundleId == nil
+        guard (!hasIDSuffixAndBundleID && !hasNoIDSuffixOrBundleID) || name == "default" else {
+            throw RuntimeError(
+                """
+                Variant "\(name)" have "id_suffix" and "bundle_id" configured at the same time or no \
+                configuration were provided to any of them. Please provide only one of them per variant.
+                """)
+        }
+        
+        self.name = name
+        self.versionName = unnamediOSVariant.versionName
+        self.versionNumber = unnamediOSVariant.versionNumber
+        self.appIcon = unnamediOSVariant.appIcon
+        self.storeDestination = try Self.parseDestination(name: name, destination: unnamediOSVariant.storeDestination) ?? .appStore
+        self.custom = unnamediOSVariant.custom
+        self.idSuffix = unnamediOSVariant.idSuffix
+        self.bundleID = unnamediOSVariant.bundleId
+    }
+    
+    func makeBundleID(for target: iOSTarget) -> String {
+        guard bundleID == nil else { return bundleID! }
+        guard name != "default" else { return target.bundleId }
+        
+        return target.bundleId + (idSuffix ?? "")
+    }
     
     func getDefaultValues(for target: iOSTarget) -> [String: String] {
-        let bundleId = target.bundleId+configIdSuffix
-        
         var customDictionary: [String: String] = [
-            "V_APP_NAME": target.name+configName,
-            "V_BUNDLE_ID": bundleId,
-            "V_VERSION_NAME": version_name,
-            "V_VERSION_NUMBER": String(version_number),
-            "V_APP_ICON": app_icon ?? target.app_icon
+            "V_APP_NAME": target.name + configName,
+            "V_BUNDLE_ID": makeBundleID(for: target),
+            "V_VERSION_NAME": versionName,
+            "V_VERSION_NUMBER": String(versionNumber),
+            "V_APP_ICON": appIcon ?? target.app_icon
         ]
        
-        if
-            signing?.matchURL != nil,
-            let exportMethod = signing?.exportMethod {
-            customDictionary["V_MATCH_PROFILE"] = exportMethod.prefix+" "+bundleId
+        if signing?.matchURL != nil, let exportMethod = signing?.exportMethod {
+            customDictionary["V_MATCH_PROFILE"] = "\(exportMethod.prefix) \(makeBundleID(for: target))"
         }
         
         custom?
             .filter { $0.destination == .project && !$0.isEnvironmentVariable }
-            .forEach({ config in
-                customDictionary[config.name] = config.value
-            })
+            .forEach { customDictionary[$0.name] = $0.value }
         
         return customDictionary
     }
     
-    var configName: String {
-        switch name {
-        case "default":
-            return ""
-        default:
-            return " "+name
+    private static func parseDestination(name: String, destination: String?) throws -> Destination? {
+        guard let destinationString = destination else { return nil }
+        
+        guard let destination = Destination(rawValue: destinationString) else {
+            throw RuntimeError(
+                """
+                Variant "\(name)" provided an invalid destination. Please choose between \
+                \(Destination.allCases.map({ $0.rawValue }).joined(separator: ", "))
+                """)
         }
-    }
-    
-    var configIdSuffix: String {
-        switch name {
-        case "default":
-            return ""
-        default:
-            return id_suffix != nil ? "."+id_suffix! : ""
-        }
-    }
-    
-    var destinationProperty: CustomProperty {
-        var defaultDestination: Destination = .appStore
-        if
-            let storeDestination = store_destination,
-            let destination = Destination(rawValue: storeDestination.lowercased()) {
-            defaultDestination = destination
-        }
-        return CustomProperty(
-            name: "STORE_DESTINATION",
-            value: defaultDestination.rawValue,
-            destination: .fastlane
-        )
+        
+        return destination
     }
 }
 
 extension iOSVariant {
-    enum Destination: String, Codable {
+    enum Destination: String, Codable, CaseIterable {
         case appCenter = "appcenter"
         case appStore = "appstore"
         case testFlight = "testflight"
@@ -93,22 +127,35 @@ extension iOSVariant {
  * as dictionary `[String: UnnamediOSVariant]` and expose array `[iOSVariant]`.
  */
 struct UnnamediOSVariant: Codable {
-    let app_icon: String?
-    let id_suffix: String?
-    let version_name: String
-    let version_number: Int
+    let versionName: String
+    let versionNumber: Int
+    let appIcon: String?
+    let idSuffix: String?
+    let bundleId: String?
     let signing: iOSSigning?
     let custom: [CustomProperty]?
-    internal let store_destination: String?
+    let storeDestination: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case versionName = "version_name"
+        case versionNumber = "version_number"
+        case appIcon = "app_icon"
+        case idSuffix = "id_suffix"
+        case bundleId = "bundle_id"
+        case signing
+        case custom
+        case storeDestination = "store_destination"
+    }
     
     init(from decoder: Decoder) throws {
         let values = try decoder.container(keyedBy: CodingKeys.self)
-        app_icon = try values.decodeIfPresentOrReadFromEnv(String.self, forKey: .app_icon)
-        id_suffix = try values.decodeIfPresentOrReadFromEnv(String.self, forKey: .id_suffix)
-        version_name = try values.decodeOrReadFromEnv(String.self, forKey: .version_name)
-        version_number = try values.decodeOrReadFromEnv(Int.self, forKey: .version_number)
+        versionName = try values.decodeOrReadFromEnv(String.self, forKey: .versionName)
+        versionNumber = try values.decodeOrReadFromEnv(Int.self, forKey: .versionNumber)
+        appIcon = try values.decodeIfPresentOrReadFromEnv(String.self, forKey: .appIcon)
+        idSuffix = try values.decodeIfPresentOrReadFromEnv(String.self, forKey: .idSuffix)
+        bundleId = try values.decodeIfPresentOrReadFromEnv(String.self, forKey: .bundleId)
         signing = try values.decodeIfPresent(iOSSigning.self, forKey: .signing)
         custom = try values.decodeIfPresent([CustomProperty].self, forKey: .custom)
-        store_destination = try values.decodeIfPresentOrReadFromEnv(String.self, forKey: .store_destination)
+        storeDestination = try values.decodeIfPresentOrReadFromEnv(String.self, forKey: .storeDestination)
     }
 }
