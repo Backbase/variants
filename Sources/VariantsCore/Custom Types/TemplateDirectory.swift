c67cc3fd64f8dea0ad18f38117ec8f2c941438b7
Arthur Alves <arthuralves.p@gmail.com>
2022-08-05 13:30:19 +0200

    fix: inability to find 'Templates' folder when installing Variants with homebrew on Apple Silicon

--- Sources/VariantsCore/Custom Types/TemplateDirectory.swift
+++ Sources/VariantsCore/Custom Types/TemplateDirectory.swift
@@ -19,11 +19,23 @@ struct TemplateDirectory {
             "./Templates"
         ]
     ) throws {
-        let firstDirectory = directories
-            .map(Path.init(stringLiteral:))
-            .first(where: \.exists)
-
-        guard let path = firstDirectory else {
+        
+        var templateDirectories = directories.map(Path.init(stringLiteral:))
+        
+        if let variantsInstallationPath = try? Bash(
+            "which",
+            arguments: "variants"
+        ).capture() {
+            templateDirectories.append(
+                Path(variantsInstallationPath.replacingOccurrences(
+                    of: "/bin/variants",
+                    with: "/lib/variants/templates"
+                ))
+            )
+        }
+        
+        let firstFoundDirectory = templateDirectories.first(where: \.exists)
+        guard let path = firstFoundDirectory else {
             let dirs = directories.joined(separator: " or ")
             throw RuntimeError("Templates folder not found in \(dirs)")
         }
